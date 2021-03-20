3c0d337b4048969fecccaf243c069eebb396e268
den59k <den59k@gmail.com>
2021-03-20 13:39:06 +0500

    opencv-starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import CV from 'services/cv'
+import { delay } from 'services/delay'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const sourceVideoRef = useRef()
+	const canvasRef = useRef()
+	const [ fps, setFps ] = useState(0)
+
+	useEffect(() => {
+		
+		function initCanvas (width, height){
+
+			const canvas = document.createElement('canvas')
+			canvas.width = width
+			canvas.height = height
+			const ctx = canvas.getContext('2d')
+
+			return (videoElement) => {
+				ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height)
+				return ctx.getImageData(0, 0, canvas.width, canvas.height)
+			}
+		}
+
+		async function initCamera(maxVideoSize){
+
+			const stream = await navigator.mediaDevices.getUserMedia({
+				audio: false,
+				video: {
+					facingMode: 'environment',
+					width: maxVideoSize,
+					height: maxVideoSize,
+				},
+			})
+
+			sourceVideoRef.current.srcObject = stream
+			sourceVideoRef.current.play()
+
+			await delay(250)			//Верь мне, так нужно
+
+			return stream
+		}
+
+		async function initCV (){
+			const res = await CV.init()
+			return res.status
+		}
+
+		Promise.all([ initCV(), initCamera(400) ]).then(values => {
+			const getImageData = initCanvas(sourceVideoRef.current.videoWidth, sourceVideoRef.current.videoHeight)
+			
+			//Мы запускаем цикл, в котором просто будем выводить
+			async function computeImage() {
+
+				const time = performance.now()
+
+				const imageData = getImageData(sourceVideoRef.current)
+				const resultImageData = await CV.convertToGray(imageData)
+
+				canvasRef.current.width = resultImageData.width
+				canvasRef.current.height = resultImageData.height
+				const ctx = canvasRef.current.getContext('2d')
+				ctx.putImageData(resultImageData, 0, 0)
+
+				setFps(Math.round(1000 / (performance.now() - time)))
+				requestAnimationFrame(computeImage)
+			}
+
+			computeImage()
+		})
+
+	}, [])
+
+	return (
+		<div className="App">
+			<div className="fps">{fps} FPS</div>
+			<video ref={sourceVideoRef} playsInline={true} ></video>
+			<canvas ref={canvasRef}></canvas>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/services/cv.js
@@ -0,0 +1,39 @@
+class CV {
+	
+	constructor(){
+		this.worker = new Worker('/js/cv.worker.js') // load worker
+		this.waiting = {}
+		
+		this.worker.onmessage = (e) => {
+			const { action, payload } = e.data
+			if(this.waiting[action]){
+				this.waiting[action](payload)
+				delete this.waiting[action]
+			}
+		}
+
+		this.worker.onerror = (e) => {
+			console.log(e)
+		}
+	}
+	
+	//Мы промисифицируем данный метод, чтобы ожидать ответ
+	_dispatch = (action, payload) => new Promise ((res, rej) => {
+		if(this.waiting[action]) return rej (`Action ${action} is not completed`)
+
+		this.worker.postMessage({ action, payload })
+		this.waiting[action] = res
+	})
+
+
+	init() {
+		return this._dispatch("init")
+	}
+
+	convertToGray (imageData){
+		return this._dispatch("convertToGray", imageData)
+	}
+
+}
+
+export default new CV()
