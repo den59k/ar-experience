f863c672e351ad3e68bf1c1dc48dfca5ebe71283
den59k <den59k@gmail.com>
2021-03-23 02:42:51 +0500

    a

--- public/js/cv.worker.js
+++ public/js/cv.worker.js
@@ -9,7 +9,7 @@ const methods = {
 
 		cv = await cv()                                         //Таким образом мы инициализируем OpenCV
 
-		orb = new cv.ORB(300)                                   //А затем сразу создадим ORB
+		orb = new cv.ORB(500)                                   //А затем сразу создадим ORB
 		bfMatcher = new cv.BFMatcher(cv.NORM_HAMMING2, true)    //А заодно и матчер
 
 		console.log(Object.keys(cv))
