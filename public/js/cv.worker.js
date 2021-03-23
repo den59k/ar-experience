1f92cd54baf252550ee0eb7ce65aa2e33e19ba07
den59k <den59k@gmail.com>
2021-03-23 18:09:24 +0500

    a

--- public/js/cv.worker.js
+++ public/js/cv.worker.js
@@ -9,8 +9,8 @@ const methods = {
 
 		cv = await cv()                                         //Таким образом мы инициализируем OpenCV
 
-		orb = new cv.ORB(500)                                   //А затем сразу создадим ORB
-		bfMatcher = new cv.BFMatcher(cv.NORM_HAMMING2, true)    //А заодно и матчер
+		orb = new cv.ORB(700)                                   //А затем сразу создадим ORB
+		bfMatcher = new cv.BFMatcher(cv.NORM_HAMMING, true)    //А заодно и матчер
 
 		console.log(Object.keys(cv))
 
@@ -75,7 +75,7 @@ const methods = {
 			const queryImageData = memoryData[id].keypointsData
 			const trainImageData = getImageKeypoints(imgGray)
 			
-			const a = matchKeypoints(queryImageData, trainImageData, 40)
+			const a = matchKeypoints(queryImageData, trainImageData, 35)
 			
 			queryPointsMat = a.queryPointsMat
 			trainPointsMat = a.trainPointsMat
@@ -84,7 +84,7 @@ const methods = {
 		}
 
 
-		if(trainPointsMat && trainPointsMat.rows > 6){
+		if(trainPointsMat && trainPointsMat.rows > 8){
 	
 			const mtx = getCameraMatrix(imgGray.rows, imgGray.cols)
 			const dist = getDistortion()
@@ -127,163 +127,6 @@ const methods = {
 		if(memoryData[id].lastFrame) memoryData[id].lastFrame.delete()
 		memoryData[id].lastFrame = imgGray
 		
-		return imageDataFromMat(finalImage)
-	},
-
-	calculatePnP: async ({ sourceImage, imageData }) => {
-		const img = cv.matFromImageData(imageData)
-		const queryImage = sourceImages[sourceImage.id]
-		const trainImage = getImageKeypoints(img)
-
-		let queryPoints;
-		let trainPoints;
-
-		let finalImage = new cv.Mat()
-		cv.cvtColor(trainImage.image, finalImage, cv.COLOR_GRAY2RGB)
-
-		if(lastFramesData[sourceImage.id]){
-			queryPoints = lastFramesData[sourceImage.id].queryPoints
-			trainPoints = lastFramesData[sourceImage.id].trainPoints
-
-			const lastPoints = cv.matFromArray(trainPoints.length, 1, cv.CV_32FC2, trainPoints.flat());
-			const nextPoints = new cv.Mat()
-
-			const status = new cv.Mat()
-			const errors = new cv.Mat()
-
-			cv.calcOpticalFlowPyrLK(lastFramesData[sourceImage.id].image, trainImage.image, lastPoints, nextPoints, status, errors)
-	
-			trainPoints = []
-			for(let i = 0; i < nextPoints.rows; i++)
-				if(status.charAt(i) === 1 && errors.floatAt(i) < 10)
-					trainPoints.push([
-						nextPoints.floatAt(i, 0),
-						nextPoints.floatAt(i, 1)
-					])
-			
-			errors.delete()
-			status.delete()
-			nextPoints.delete()
-
-			if(trainPoints.length < 6){
-				trainPoints = null
-				queryPoints = null
-				lastFramesData[sourceImage.id] = null
-			}
-		}
-
-		if(!trainPoints){
-			const matches = new cv.DMatchVector()
-
-			if(trainImage.keypoints.size() > 5)
-				bfMatcher.match(queryImage.descriptors, trainImage.descriptors, matches)
-
-			const good_matches = []
-			for (let i = 0; i < matches.size(); i++) {
-				if (matches.get(i).distance < 30) 
-					good_matches.push(matches.get(i));
-			}
-			matches.delete()
-
-			if(good_matches.length > 8){
-				queryPoints = []
-				trainPoints = []
-				
-				for(let i = 0; i < good_matches.length; i++) {
-					queryPoints.push([
-						queryImage.keypoints.get(good_matches[i].queryIdx).pt.x,
-						queryImage.keypoints.get(good_matches[i].queryIdx).pt.y,
-						0
-					])
-
-					trainPoints.push([
-						trainImage.keypoints.get(good_matches[i].trainIdx).pt.x,
-						trainImage.keypoints.get(good_matches[i].trainIdx).pt.y
-					])
-				}
-			}
-		}
-
-		if(trainPoints){
-			const mat1 = cv.matFromArray(queryPoints.length, 1, cv.CV_32FC3, queryPoints.flat());
-			const mat2 = cv.matFromArray(trainPoints.length, 1, cv.CV_32FC2, trainPoints.flat());
-
-			const f = trainImage.image.cols/2/(Math.tan(angle/2*Math.PI/180))
-	
-			const _mtx = [
-				f, 0, trainImage.image.cols / 2,
-				0, f, trainImage.image.rows / 2 ,
-				0, 0, 1
-			]
-
-			const _dist = [ 0, 0, 0, 0 ]
-
-			const mtx = cv.matFromArray(3, 3, cv.CV_64F, _mtx)
-			const dist = cv.matFromArray(1, _dist.length, cv.CV_64F, _dist)
-
-			const rvec = new cv.Mat()
-			const tvec = new cv.Mat()
-
-			const inliers = new cv.Mat()
-			cv.solvePnPRansac(mat1, mat2, mtx, dist, rvec, tvec, false, 100, 8.0, 0.99, inliers)
-	
-			if(inliers.rows / mat1.rows > 0.8){
-				const projectionMatrix = getProjectionMatrix(rvec, tvec, mtx)
-
-				const storePoints = { trainPoints: [], queryPoints: [] }
-				for(let i = 0; i < inliers.rows; i++){
-					storePoints.trainPoints.push(trainPoints[inliers.intAt(i, 0)])
-					storePoints.queryPoints.push(queryPoints[inliers.intAt(i, 0)])
-				}
-				lastFramesData[sourceImage.id] = storePoints
-
-				mtx.delete()
-				dist.delete()
-
-				const _axis = [
-					0, 0, 0, 1,
-					30, 0, 0, 1,
-					0, 30, 0, 1,
-					0, 0, -30, 1
-				]
-				const axis = cv.matFromArray(4, 4, cv.CV_64F, _axis).t()
-
-				const points = dot(projectionMatrix, axis).t()
-
-				const pointsArr = []
-				for(let i = 0; i < 4; i++){
-					pointsArr.push({
-						x: points.doubleAt(i, 0) / points.doubleAt(i, 2),
-						y: points.doubleAt(i, 1) / points.doubleAt(i, 2)
-					})
-				}
-
-		
-				cv.line(finalImage, pointsArr[0], pointsArr[1], [ 255, 0, 0, 255 ], 2 )
-				cv.line(finalImage, pointsArr[0], pointsArr[2], [ 0, 255, 0, 255 ], 2 )
-				cv.line(finalImage, pointsArr[0], pointsArr[3], [ 0, 0, 255, 255 ], 2 )
-
-				for(let point of storePoints.trainPoints)
-					cv.circle(finalImage, { x: point[0], y: point[1] }, 2, [ 0, 0, 255, 255 ])
-				
-				projectionMatrix.delete()
-				axis.delete()
-				points.delete()
-			}else{
-				lastFramesData[sourceImage.id] = null
-			}
-
-			mat1.delete()
-			mat2.delete()
-		}
-		
-		if(lastFramesData[sourceImage.id]){
-			if(lastFramesData[sourceImage.id].image)
-				lastFramesData[sourceImage.id].image.delete()
-			lastFramesData[sourceImage.id].image = new cv.Mat(trainImage.image)
-		}
-
-		trainImage.delete()
 		return imageDataFromMat(finalImage)
 	},
 
@@ -498,6 +341,7 @@ function imageDataFromMat(mat) {
 	mat.delete()
 	return clampedArray
 }
+
 
 //В этой функции мы просто вызываем нужный метод и возвращаем ответ
 onmessage = async function(e) {
